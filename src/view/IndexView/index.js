b899a12a8e6740f3631fd594392eaadaa690b96d
Alvaro <AlvSag@gmail.com>
2022-05-27 10:50:31 +0200

    Add  structure

---
+++ src/view/IndexView/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.scss';
+
+export const IndexView = () => {
+    return (
+        <>
+            <h2 className="landing_title">GeekHubs view </h2>
+        </>
+    )
+}
